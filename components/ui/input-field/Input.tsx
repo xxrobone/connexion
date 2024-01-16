4d3e42b9d0fbbf5e519f0d726c073fad2d8504e3
robonexx <robertwagar@gmail.com>
2024-01-16 14:10:36 +0100

    input field

---
+++ components/ui/input-field/Input.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent } from 'react'
+
+type InputFieldType = {
+    htmlFor: string
+    id: string
+    labelValue: string
+    name: string
+    type: string
+    handleForm: (e: ChangeEvent<HTMLInputElement>) => void
+  }
+
+const InputField = ({
+  htmlFor,
+  id,
+  labelValue,
+  name,
+  type,
+  handleForm,
+}: InputFieldType) => {
+  return (
+    <div className='flex gap-2 border-b'>
+      <label htmlFor={htmlFor}>{labelValue}</label>
+      <input
+        className='focus:outline-none'
+        type={type}
+        id={id}
+        name={name}
+        onChange={handleForm}
+      />
+    </div>
+  )
+}
+
+export default InputField
