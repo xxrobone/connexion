bbeb6480f7756219d8c3c7632b7fa2e4714aaf9a
robonexx <robertwagar@gmail.com>
2024-01-16 16:57:06 +0100

    padding to user list

--- app/dashboard/users/components/users-list/UsersList.tsx
+++ app/dashboard/users/components/users-list/UsersList.tsx
@@ -48,7 +48,7 @@ const mockData = [
 
 const UsersList = () => {
   return (
-    <div className='relative overflow-x-auto h-full'>
+    <div className='relative overflow-x-auto h-full p-20'>
       <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
         <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
           <tr>
