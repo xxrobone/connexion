df233bd2bf717685cba8ec352ec4fbfdc832e742
robonexx <robertwagar@gmail.com>
2024-01-16 17:02:55 +0100

    working on image avatar

---
+++ components/avatar/Avatar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type ImageProps = {
+    profileImg: string | HTMLImageElement
+}
+
+const Avatar = ({profileImg}: ImageProps) => {
+  return (
+    <Image>Avatar</Image>
+  )
+}
+
+export default Avatar
