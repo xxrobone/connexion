9faa99cc08ba3c10b72465b06c3c80df7695b07e
robonexx <robertwagar@gmail.com>
2024-01-16 14:08:49 +0100

    search bar

--- components/ui/search-bar/SearchBar.tsx
+++ components/ui/search-bar/SearchBar.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { LiaSearchSolid } from "react-icons/lia";
 
 const SearchBar = () => {
   return (
-    <div className="w-fit h-[50px] bg-neutral-700 rounded-[30px] "></div>
+    <div className="w-fit h-[50px] bg-neutral-700 rounded-[30px] "><LiaSearchSolid /><input></input></div>
   )
 }
 
