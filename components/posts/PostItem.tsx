2f56291cb18c1ffb7d6d77f6e0d2a6941bdfd062
robonexx <robertwagar@gmail.com>
2024-01-17 12:56:04 +0100

    added tag component to postitem

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Tag from '../ui/tag/Tag';
 
 interface PostItemProps {
-    date: string;
-    title: string;
-    tags: string[];
-    desc: string;
-  }
+  date: string;
+  title: string;
+  tags: string[];
+  desc: string;
+}
 
-  const PostItem: React.FC<PostItemProps> = ({ date, title, tags, desc }) => {
+const PostItem: React.FC<PostItemProps> = ({ date, title, tags, desc }) => {
   return (
     <div className='py-5'>
       <article className='flex flex-col space-y-2 xl:space-y-0'>
@@ -24,24 +25,25 @@ interface PostItemProps {
                 className='text-gray-900 dark:text-black drop-shadow-[1px_2px_1px_rgba(255,64,64.25)]'
                 href='/blog/release-of-tailwind-nextjs-starter-blog-v2.0'
               >
-               {title}
+                {title}
               </a>
             </h2>
             <div className='flex flex-wrap'>
-            {tags.map((tag, index) => (
-                <a
+              {tags.map((tag, index) => (
+                /*  <a
                   key={index}
                   className='group mr-3 p-1 rounded-sm text-sm font-medium uppercase text-primary-500 hover:text-white hover:bg-zinc-800 dark:hover:text-primary-400 font-alt transition-all duration-300'
                   href={`/tags/${tag.toLowerCase()}`}
                 >
                     {tag}
                     <span className='block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#ff4040] opacity-50'></span>
-                </a>
+                </a> */
+                <Tag key={index} tag={tag} />
               ))}
             </div>
           </div>
           <div className='prose max-w-none text-gray-500 dark:text-gray-600'>
-                     {desc}
+            {desc}
           </div>
         </div>
       </article>
